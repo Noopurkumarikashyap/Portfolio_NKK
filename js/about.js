b18061a91f7ad38f65e97ba458f136c8fe5e72aa
Noopur Kumari Kashyap <64243015+Noopurkumarikashyap@users.noreply.github.com>
2026-05-12 13:01:06 +0530

    Update about.js

--- js/about.js
+++ js/about.js
@@ -49,23 +49,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     isAnimating = true;
 
-    /* POP FIRST */
+    /* REMOVE OLD CLASS FIRST */
+    letter.classList.remove("pop-out");
 
+    /* FORCE REFLOW */
+    void letter.offsetWidth;
+
+    /* POP UP */
     isPopped = true;
 
-    /* SHINE COMES AFTER POP */
-
+    /* ADD GLOW AFTER POP */
     setTimeout(() => {
 
       letter.classList.add("pop-out");
 
     }, 180);
 
-    /* RESET */
-
+    /* RETURN BACK */
     setTimeout(() => {
 
       isPopped = false;
+
+    }, 1200);
+
+    /* REMOVE SHINE */
+    setTimeout(() => {
 
       letter.classList.remove("pop-out");
 
@@ -89,21 +97,29 @@ document.addEventListener("DOMContentLoaded", () => {
     /* PHOTO */
 
     photo.style.transform = `
-      translate(${currentX}px, ${currentY}px)
+      translate3d(
+        ${currentX}px,
+        ${currentY}px,
+        0
+      )
       rotate(8deg)
     `;
 
     /* LETTER */
 
     const popY = isPopped ? -130 : 0;
+
     const popScale = isPopped ? 1.03 : 1;
 
+    const popRotate = isPopped ? -2deg : -4deg;
+
     letter.style.transform = `
-      translate(
+      translate3d(
         ${currentX * 0.6}px,
-        ${currentY * 0.6 + popY}px
+        ${currentY * 0.6 + popY}px,
+        0
       )
-      rotate(-4deg)
+      rotate(${isPopped ? -2 : -4}deg)
       scale(${popScale})
     `;
 
